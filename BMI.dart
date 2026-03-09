9d2162d42611945534c5542cb9febe089ebf0f19
dinaameil <dinaameil753@gmail.com>
2026-03-09 22:49:20 +0700

    Menambah Stuktur Datar BMI dan Menambah Validasi

---
+++ BMI.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  List<String> riwayat = [];
+  bool lanjut = true;
+
+  print("=== APLIKASI KALKULATOR BMI ===");
+
+  while (lanjut) {
+    stdout.write("\nMasukkan Berat Badan (kg): ");
+    double? berat = double.tryParse(stdin.readLineSync()!);
+
+    stdout.write("Masukkan Tinggi Badan (cm): ");
+    double? tinggiCm = double.tryParse(stdin.readLineSync()!);
+
+    // Validasi Input (Branch input-validation)
+    if (berat == null || tinggiCm == null || berat <= 0 || tinggiCm <= 0) {
+      print("Input tidak valid! Harap masukkan angka positif.");
+      continue;
+    }
+  }
+}  
